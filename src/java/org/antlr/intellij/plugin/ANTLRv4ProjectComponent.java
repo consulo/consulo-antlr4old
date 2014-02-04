8edf9bb8a0f4a95dedf51a5d51ce789a3f1b22b2
VISTALL <vistall.valeriy@gmail.com>
2014-02-04 11:39:34 +0200

    #CEP-30 fixed

--- src/java/org/antlr/intellij/plugin/ANTLRv4ProjectComponent.java
+++ src/java/org/antlr/intellij/plugin/ANTLRv4ProjectComponent.java
@@ -1,11 +1,9 @@
 package org.antlr.intellij.plugin;
 
-import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
+import java.io.IOException;
+
+import javax.swing.JTextArea;
+
 import org.antlr.intellij.plugin.preview.ParseTreePanel;
 import org.antlr.v4.Tool;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -24,89 +22,101 @@ import org.antlr.v4.tool.LexerGrammar;
 import org.antlr.v4.tool.ast.GrammarRootAST;
 import org.jetbrains.annotations.NotNull;
 import org.stringtemplate.v4.ST;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VirtualFile;
 
-import javax.swing.*;
-import java.io.IOException;
-
-public class ANTLRv4ProjectComponent implements ProjectComponent {
+public class ANTLRv4ProjectComponent implements ProjectComponent
+{
 	public ParseTreePanel treePanel;
 	public Project project;
 
-	public ANTLRv4ProjectComponent(Project project) {
+	public ANTLRv4ProjectComponent(Project project)
+	{
 		this.project = project;
 	}
 
-	public static ANTLRv4ProjectComponent getInstance(Project project) {
-		ANTLRv4ProjectComponent pc = project.getComponent(ANTLRv4ProjectComponent.class);
-		return pc;
+	public static ANTLRv4ProjectComponent getInstance(Project project)
+	{
+		return project.getComponent(ANTLRv4ProjectComponent.class);
 	}
 
-	public static Project getProjectForFile(VirtualFile virtualFile) {
+	public static Project getProjectForFile(VirtualFile virtualFile)
+	{
 		Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
 		Project project = null;
-		for (int i = 0; i < openProjects.length; i++) {
+		for(int i = 0; i < openProjects.length; i++)
+		{
 			Project p = openProjects[i];
 			ProjectFileIndex fileIndex = ProjectRootManager.getInstance(p).getFileIndex();
-			if ( fileIndex.isInContent(virtualFile) ) {
+			if(fileIndex.isInContent(virtualFile))
+			{
 				project = p;
 			}
 		}
 		return project;
 	}
 
-	public ParseTreePanel getViewerPanel() {
+	public ParseTreePanel getViewerPanel()
+	{
 		return treePanel;
 	}
 
 	// -------------------------------------
 
 	@Override
-	public void initComponent() {
+	public void initComponent()
+	{
 	}
 
 	@Override
-	public void projectOpened() {
+	public void projectOpened()
+	{
 		treePanel = new ParseTreePanel();
 	}
 
 	@Override
-	public void projectClosed() {
+	public void projectClosed()
+	{
 	}
 
 	@Override
-	public void disposeComponent() {
+	public void disposeComponent()
+	{
+		Disposer.dispose(treePanel);
 	}
 
 	@NotNull
 	@Override
-	public String getComponentName() {
+	public String getComponentName()
+	{
 		return "antlr.ProjectComponent";
 	}
 
-//	private ToolWindow getToolWindow()
-//	{
-//		ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(_project);
-//		ToolWindow toolWindow = toolWindowManager.getToolWindow(ParseTreeWindowFactory.ID);
-//		if ( toolWindow!=null ) {
-//			return toolWindow;
-//		}
-//		else {
-//			return toolWindowManager.registerToolWindow(ID_TOOL_WINDOW,
-//														_viewerPanel,
-//														ToolWindowAnchor.RIGHT);
-//		}
-//	}
-//
-//	private boolean isToolWindowRegistered()
-//	{
-//		return ToolWindowManager.getInstance(_project).getToolWindow(ID_TOOL_WINDOW) != null;
-//	}
+	//	private ToolWindow getToolWindow()
+	//	{
+	//		ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(_project);
+	//		ToolWindow toolWindow = toolWindowManager.getToolWindow(ParseTreeWindowFactory.ID);
+	//		if ( toolWindow!=null ) {
+	//			return toolWindow;
+	//		}
+	//		else {
+	//			return toolWindowManager.registerToolWindow(ID_TOOL_WINDOW,
+	//														_viewerPanel,
+	//														ToolWindowAnchor.RIGHT);
+	//		}
+	//	}
+	//
+	//	private boolean isToolWindowRegistered()
+	//	{
+	//		return ToolWindowManager.getInstance(_project).getToolWindow(ID_TOOL_WINDOW) != null;
+	//	}
 
-	public static Object[] parseText(ParseTreePanel parseTreePanel,
-									 String inputText,
-									 String grammarFileName,
-									 String startRule)
-		throws IOException
+	public static Object[] parseText(ParseTreePanel parseTreePanel, String inputText, String grammarFileName, String startRule) throws IOException
 	{
 		Tool antlr = new Tool();
 		antlr.errMgr = new PluginIgnoreMissingTokensFileErrorManager(antlr);
@@ -116,39 +126,48 @@ public class ANTLRv4ProjectComponent implements ProjectComponent {
 		String combinedGrammarFileName = null;
 		String lexerGrammarFileName = null;
 		String parserGrammarFileName = null;
-		if ( grammarFileName.contains("Lexer") ) {
+		if(grammarFileName.contains("Lexer"))
+		{
 			lexerGrammarFileName = grammarFileName;
 			int i = grammarFileName.indexOf("Lexer");
-			parserGrammarFileName = grammarFileName.substring(0,i)+"Parser.g4";
+			parserGrammarFileName = grammarFileName.substring(0, i) + "Parser.g4";
 		}
-		else if ( grammarFileName.contains("Parser") ) {
+		else if(grammarFileName.contains("Parser"))
+		{
 			parserGrammarFileName = grammarFileName;
 			int i = grammarFileName.indexOf("Parser");
-			lexerGrammarFileName = grammarFileName.substring(0,i)+"Lexer.g4";
+			lexerGrammarFileName = grammarFileName.substring(0, i) + "Lexer.g4";
 		}
-		else {
+		else
+		{
 			combinedGrammarFileName = grammarFileName;
 		}
 
 		ANTLRInputStream input = new ANTLRInputStream(inputText);
 		LexerInterpreter lexEngine;
 		Grammar g;
-		if ( combinedGrammarFileName!=null ) {
+		if(combinedGrammarFileName != null)
+		{
 			g = antlr.loadGrammar(grammarFileName);
-			if ( listener.grammarErrorMessage!=null ) {
+			if(listener.grammarErrorMessage != null)
+			{
 				return null;
 			}
 			lexEngine = g.createLexerInterpreter(input);
 		}
-		else {
+		else
+		{
 			LexerGrammar lg = null;
-			try {
-				lg = (LexerGrammar)Grammar.load(lexerGrammarFileName);
+			try
+			{
+				lg = (LexerGrammar) Grammar.load(lexerGrammarFileName);
 			}
-			catch (ClassCastException cce) {
-				System.err.println("File "+lexerGrammarFileName+" isn't a lexer grammar");
+			catch(ClassCastException cce)
+			{
+				System.err.println("File " + lexerGrammarFileName + " isn't a lexer grammar");
 			}
-			if ( listener.grammarErrorMessage!=null ) {
+			if(listener.grammarErrorMessage != null)
+			{
 				return null;
 			}
 			g = loadGrammar(antlr, parserGrammarFileName, lg);
@@ -165,14 +184,21 @@ public class ANTLRv4ProjectComponent implements ProjectComponent {
 		parser.addErrorListener(syntaxErrorListener);
 		ParseTree t = parser.parse(g.getRule(startRule).index);
 		console.setText(syntaxErrorListener.syntaxError);
-		if ( t!=null ) {
-			return new Object[] {parser, t};
+		if(t != null)
+		{
+			return new Object[]{
+					parser,
+					t
+			};
 		}
 		return null;
 	}
 
-	/** Same as loadGrammar(fileName) except import vocab from existing lexer */
-	public static Grammar loadGrammar(Tool tool, String fileName, LexerGrammar lexerGrammar) {
+	/**
+	 * Same as loadGrammar(fileName) except import vocab from existing lexer
+	 */
+	public static Grammar loadGrammar(Tool tool, String fileName, LexerGrammar lexerGrammar)
+	{
 		GrammarRootAST grammarRootAST = tool.parseGrammar(fileName);
 		final Grammar g = tool.createGrammar(grammarRootAST);
 		g.fileName = fileName;
@@ -181,30 +207,37 @@ public class ANTLRv4ProjectComponent implements ProjectComponent {
 		return g;
 	}
 
-	static class MyANTLRToolListener extends DefaultToolListener {
+	static class MyANTLRToolListener extends DefaultToolListener
+	{
 		public String grammarErrorMessage;
-		public MyANTLRToolListener(Tool tool) { super(tool); }
+
+		public MyANTLRToolListener(Tool tool)
+		{
+			super(tool);
+		}
 
 		@Override
-		public void error(ANTLRMessage msg) {
-//			super.error(msg);
+		public void error(ANTLRMessage msg)
+		{
+			//			super.error(msg);
 			ST msgST = tool.errMgr.getMessageTemplate(msg);
 			grammarErrorMessage = msgST.render();
-			if (tool.errMgr.formatWantsSingleLineMessage()) {
+			if(tool.errMgr.formatWantsSingleLineMessage())
+			{
 				grammarErrorMessage = grammarErrorMessage.replace('\n', ' ');
 			}
 		}
 	}
 
-	static class MyConsoleErrorListener extends ConsoleErrorListener {
-		public String syntaxError="";
+	static class MyConsoleErrorListener extends ConsoleErrorListener
+	{
+		public String syntaxError = "";
+
 		@Override
-		public void syntaxError(Recognizer<?, ?> recognizer,
-								@Nullable Object offendingSymbol,
-								int line, int charPositionInLine, String msg,
-								@Nullable RecognitionException e)
+		public void syntaxError(Recognizer<?, ?> recognizer, @Nullable Object offendingSymbol, int line, int charPositionInLine, String msg,
+				@Nullable RecognitionException e)
 		{
-//			super.syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
+			//			super.syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e);
 			syntaxError = "line " + line + ":" + charPositionInLine + " " + msg;
 		}
 	}
--- src/java/org/antlr/intellij/plugin/preview/ParseTreePanel.java
+++ src/java/org/antlr/intellij/plugin/preview/ParseTreePanel.java
@@ -1,5 +1,22 @@
 package org.antlr.intellij.plugin.preview;
 
+import java.awt.BorderLayout;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.antlr.intellij.plugin.ANTLRv4ProjectComponent;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.TreeViewer;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -7,80 +24,78 @@ import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.ui.Splitter;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
-import org.antlr.intellij.plugin.ANTLRv4ProjectComponent;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.gui.TreeViewer;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.*;
-import java.io.IOException;
-import java.util.Arrays;
-
-public class ParseTreePanel extends JPanel {
+public class ParseTreePanel extends JPanel implements Disposable
+{
 	protected TreeViewer viewer;  // the antlr tree viewer component itself
 	protected JLabel startRuleLabel;
 	protected Editor editor;
 	protected JTextArea console;
 	protected String inputText = "";
-	/** Either combined file or XParser or XLexer. If not combined, look for
-	 *  other with appropriate Parser or Lexer suffix.
+	/**
+	 * Either combined file or XParser or XLexer. If not combined, look for
+	 * other with appropriate Parser or Lexer suffix.
 	 */
 	protected String grammarFileName;
 	protected String startRule;
 
-	public ParseTreePanel() {
+	public ParseTreePanel()
+	{
 		buildGUI();
 	}
 
-	public void buildGUI() {
+	public void buildGUI()
+	{
 		this.setLayout(new BorderLayout());
 
 		// wrap tree and slider in panel
-		JPanel treePanel = new JPanel(new BorderLayout(0,0));
-		treePanel.setBackground(Color.white);
+		JPanel treePanel = new JPanel(new BorderLayout(0, 0));
+		treePanel.setBackground(JBColor.WHITE);
 		// Wrap tree viewer component in scroll pane
 		viewer = new TreeViewer(null, null);
 		JScrollPane scrollPane = new JBScrollPane(viewer); // use Intellij's scroller
 		treePanel.add(scrollPane, BorderLayout.CENTER);
 
 		// Add scale slider to bottom, under tree view scroll panel
-		int sliderValue = (int) ((viewer.getScale()-1.0) * 1000);
-		final JSlider scaleSlider = new JSlider(JSlider.HORIZONTAL,
-										  -999,1000,sliderValue);
-		scaleSlider.addChangeListener(
-			new ChangeListener() {
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					int v = scaleSlider.getValue();
-					viewer.setScale(v / 1000.0 + 1.0);
-				}
+		int sliderValue = (int) ((viewer.getScale() - 1.0) * 1000);
+		final JSlider scaleSlider = new JSlider(JSlider.HORIZONTAL, -999, 1000, sliderValue);
+		scaleSlider.addChangeListener(new ChangeListener()
+		{
+			@Override
+			public void stateChanged(ChangeEvent e)
+			{
+				int v = scaleSlider.getValue();
+				viewer.setScale(v / 1000.0 + 1.0);
 			}
-									 );
+		});
 		treePanel.add(scaleSlider, BorderLayout.SOUTH);
 
 		final EditorFactory factory = EditorFactory.getInstance();
 		Document doc = factory.createDocument(inputText);
 		editor = factory.createEditor(doc);
 
-		doc.addDocumentListener(new DocumentListener() {
+		doc.addDocumentListener(new DocumentListener()
+		{
 			@Override
-			public void beforeDocumentChange(DocumentEvent event) {}
+			public void beforeDocumentChange(DocumentEvent event)
+			{
+			}
+
 			@Override
-			public void documentChanged(DocumentEvent event) {
+			public void documentChanged(DocumentEvent event)
+			{
 				Document doc = event.getDocument();
 				String newText = doc.getText();
-//				System.out.println("CHANGED: " + newText);
+				//				System.out.println("CHANGED: " + newText);
 				setInput(newText);
 			}
 		});
 
 		console = new JTextArea();
 
-		JPanel editorPanel = new JPanel(new BorderLayout(0,0));
+		JPanel editorPanel = new JPanel(new BorderLayout(0, 0));
 		startRuleLabel = new JLabel("Start rule: <select from navigator or grammar>");
 		editorPanel.add(startRuleLabel, BorderLayout.NORTH);
 		editorPanel.add(editor.getComponent(), BorderLayout.CENTER);
@@ -93,80 +108,99 @@ public class ParseTreePanel extends JPanel {
 		this.add(splitPane, BorderLayout.CENTER);
 	}
 
-	public void setStartRule(String startRule) {
+	public void setStartRule(String startRule)
+	{
 		this.startRule = startRule;
-		if ( startRule==null ) {
+		if(startRule == null)
+		{
 			startRule = "<select from navigator or grammar>";
 		}
-		startRuleLabel.setText("Start rule: "+startRule);
+		startRuleLabel.setText("Start rule: " + startRule);
 	}
 
-	public void refresh() {
+	public void refresh()
+	{
 		setInputAndGrammar(inputText, grammarFileName, startRule);
 	}
 
-	public void setInput(String inputText) {
+	public void setInput(String inputText)
+	{
 		this.inputText = inputText;
 		setInputAndGrammar(inputText, grammarFileName, startRule);
 	}
 
-	public void setInputAndGrammar(final String inputText,
-								   final String grammarFileName,
-								   final String startRule)
+	public void setInputAndGrammar(final String inputText, final String grammarFileName, final String startRule)
 	{
 		this.grammarFileName = grammarFileName;
 		setStartRule(startRule);
 
-		if ( grammarFileName==null || startRule==null ) {
+		if(grammarFileName == null || startRule == null)
+		{
 			return;
 		}
 
 		final ParseTreePanel self = this;
-		ApplicationManager.getApplication().invokeLater(new Runnable() {
+		ApplicationManager.getApplication().invokeLater(new Runnable()
+		{
 			@Override
-			public void run() {
-				try {
-					Object[] results =
-						ANTLRv4ProjectComponent.parseText(self,
-														  inputText,
-														  grammarFileName,
-														  startRule);
-					if (results != null) {
+			public void run()
+			{
+				try
+				{
+					Object[] results = ANTLRv4ProjectComponent.parseText(self, inputText, grammarFileName, startRule);
+					if(results != null)
+					{
 						Parser parser = (Parser) results[0];
 						ParseTree root = (ParseTree) results[1];
 						viewer.setRuleNames(Arrays.asList(parser.getRuleNames()));
 						viewer.setTree(root);
-					} else {
+					}
+					else
+					{
 						viewer.setTree(null);
 					}
-				} catch (IOException ioe) {
+				}
+				catch(IOException ioe)
+				{
 					ioe.printStackTrace();
 				}
 			}
 		});
 	}
 
-	public TreeViewer getViewer() {
+	public TreeViewer getViewer()
+	{
 		return viewer;
 	}
 
-	public String getGrammarFileName() {
+	public String getGrammarFileName()
+	{
 		return grammarFileName;
 	}
 
-	public String getInputText() {
+	public String getInputText()
+	{
 		return inputText;
 	}
 
-	public String getStartRule() {
+	public String getStartRule()
+	{
 		return startRule;
 	}
 
-	public Editor getEditor() {
+	public Editor getEditor()
+	{
 		return editor;
 	}
 
-	public JTextArea getConsole() {
+	public JTextArea getConsole()
+	{
 		return console;
+	}
+
+	@Override
+	public void dispose()
+	{
+		EditorFactory.getInstance().releaseEditor(editor);
 	}
 }
