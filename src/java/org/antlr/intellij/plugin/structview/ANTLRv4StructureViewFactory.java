a836992b5aee3079f1ce7e4b4472d424b241085f
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 22:47:39 +0200

    compilation fixed

--- src/java/org/antlr/intellij/plugin/structview/ANTLRv4StructureViewFactory.java
+++ src/java/org/antlr/intellij/plugin/structview/ANTLRv4StructureViewFactory.java
@@ -1,24 +1,29 @@
 package org.antlr.intellij.plugin.structview;
 
+import org.antlr.intellij.plugin.ANTLRv4FileRoot;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.antlr.intellij.plugin.ANTLRv4FileRoot;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class ANTLRv4StructureViewFactory implements PsiStructureViewFactory {
-    @Nullable
-    @Override
-    public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-        return new TreeBasedStructureViewBuilder() {
-            @NotNull
-            @Override
-            public StructureViewModel createStructureViewModel() {
-                return new ANTLRv4StructureViewModel((ANTLRv4FileRoot)psiFile);
-            }
-        };
-    }
+public class ANTLRv4StructureViewFactory implements PsiStructureViewFactory
+{
+	@Nullable
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@NotNull
+			@Override
+			public StructureViewModel createStructureViewModel(@Nullable Editor editor)
+			{
+				return new ANTLRv4StructureViewModel((ANTLRv4FileRoot) psiFile);
+			}
+		};
+	}
 }
