ca7f57cd0f70ac23c7072e574e728e25a0ccaf49
VISTALL <vistall.valeriy@gmail.com>
2014-02-16 05:05:26 +0200

    #CEP-33 fixed

--- src/java/org/antlr/intellij/plugin/actions/TestRuleAction.java
+++ src/java/org/antlr/intellij/plugin/actions/TestRuleAction.java
@@ -1,5 +1,13 @@
 package org.antlr.intellij.plugin.actions;
 
+import java.util.List;
+
+import org.antlr.intellij.plugin.ANTLRv4FileRoot;
+import org.antlr.intellij.plugin.ANTLRv4ProjectComponent;
+import org.antlr.intellij.plugin.preview.ParseTreePanel;
+import org.antlr.intellij.plugin.preview.ParseTreeWindowFactory;
+import org.antlr.intellij.plugin.psi.ParserRuleRefNode;
+import org.antlr.intellij.plugin.psi.ParserRuleSpecNode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -8,6 +16,7 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
@@ -18,27 +27,30 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.messages.MessageBusConnection;
-import org.antlr.intellij.plugin.ANTLRv4FileRoot;
-import org.antlr.intellij.plugin.ANTLRv4ProjectComponent;
-import org.antlr.intellij.plugin.preview.ParseTreePanel;
-import org.antlr.intellij.plugin.preview.ParseTreeWindowFactory;
-import org.antlr.intellij.plugin.psi.ParserRuleRefNode;
-import org.antlr.intellij.plugin.psi.ParserRuleSpecNode;
 
-import java.util.List;
-
-public class TestRuleAction extends AnAction implements DumbAware {
+public class TestRuleAction extends AnAction implements DumbAware
+{
 	BulkFileListener.Adapter fileSaveListener;
 
-	/** Only show if selection is a grammar */
+	/**
+	 * Only show if selection is a grammar
+	 */
 	@Override
-	public void update(AnActionEvent e) {
+	public void update(AnActionEvent e)
+	{
 		VirtualFile file = getGrammarFile(e);
-		boolean grammarFound = file!=null;
+		boolean grammarFound = file != null;
 
 		PsiElement selectedPsiRuleNode = e.getData(LangDataKeys.PSI_ELEMENT);
-		if ( selectedPsiRuleNode==null ) return; // we clicked somewhere outside text
+		if(selectedPsiRuleNode == null)
+		{
+			return; // we clicked somewhere outside text
+		}
 		String ruleName = selectedPsiRuleNode.getText();
+		if(StringUtil.isEmpty(ruleName))
+		{
+			return;
+		}
 		boolean parserRuleFound = Character.isLowerCase(ruleName.charAt(0));
 
 		// enable action if we're looking at grammar file
@@ -48,36 +60,48 @@ public class TestRuleAction extends AnAction implements DumbAware {
 	}
 
 	@Override
-	public void actionPerformed(final AnActionEvent e) {
-//		System.out.println("exec "+e);
+	public void actionPerformed(final AnActionEvent e)
+	{
+		//		System.out.println("exec "+e);
 		Project project = getEventProject(e);
-		if ( project==null ) return; // whoa!
+		if(project == null)
+		{
+			return; // whoa!
+		}
 		PsiElement selectedPsiRuleNode = e.getData(LangDataKeys.PSI_ELEMENT);
-		if ( selectedPsiRuleNode==null ) return; // we clicked somewhere outside text
+		if(selectedPsiRuleNode == null)
+		{
+			return; // we clicked somewhere outside text
+		}
 		String ruleName = selectedPsiRuleNode.getText();
-		if ( selectedPsiRuleNode instanceof ParserRuleSpecNode ) {
-			ParserRuleRefNode r = PsiTreeUtil.findChildOfType(selectedPsiRuleNode,
-															  ParserRuleRefNode.class);
+		if(selectedPsiRuleNode instanceof ParserRuleSpecNode)
+		{
+			ParserRuleRefNode r = PsiTreeUtil.findChildOfType(selectedPsiRuleNode, ParserRuleRefNode.class);
 			ruleName = r.getText();
 		}
 
 		VirtualFile file = getGrammarFile(e);
-		if ( file==null ) return; // no files?
+		if(file == null)
+		{
+			return; // no files?
+		}
 
 		FileDocumentManager docMgr = FileDocumentManager.getInstance();
 		Document doc = docMgr.getDocument(file);
 		docMgr.saveDocument(doc);
 
-		final ParseTreePanel viewerPanel =
-			ANTLRv4ProjectComponent.getInstance(project).getViewerPanel();
+		final ParseTreePanel viewerPanel = ANTLRv4ProjectComponent.getInstance(project).getViewerPanel();
 		String inputText = viewerPanel.getInputText(); // reuse input if any is around already
 		viewerPanel.setInputAndGrammar(inputText, file.getPath(), ruleName);
 
-		if ( fileSaveListener==null ) {
-			fileSaveListener = new BulkFileListener.Adapter() {
+		if(fileSaveListener == null)
+		{
+			fileSaveListener = new BulkFileListener.Adapter()
+			{
 				@Override
-				public void after(List<? extends VFileEvent> events) {
-//					System.out.println("file changed");
+				public void after(List<? extends VFileEvent> events)
+				{
+					//					System.out.println("file changed");
 					viewerPanel.refresh();
 				}
 			};
@@ -90,15 +114,24 @@ public class TestRuleAction extends AnAction implements DumbAware {
 		toolWindow.show(null);
 	}
 
-	public VirtualFile getGrammarFile(AnActionEvent e) {
+	public VirtualFile getGrammarFile(AnActionEvent e)
+	{
 		Project project = getEventProject(e);
-		if ( project==null ) return null; // whoa!
+		if(project == null)
+		{
+			return null; // whoa!
+		}
 		VirtualFile[] files = LangDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
-		if ( files==null ) return null; // no files?
+		if(files == null)
+		{
+			return null; // no files?
+		}
 		PsiManager manager = PsiManager.getInstance(project);
-		for (VirtualFile file : files) {
-//			System.out.println(file);
-			if ( manager.findFile(file) instanceof ANTLRv4FileRoot) {
+		for(VirtualFile file : files)
+		{
+			//			System.out.println(file);
+			if(manager.findFile(file) instanceof ANTLRv4FileRoot)
+			{
 				return file;
 			}
 		}
